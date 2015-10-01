89dacc9c4838f59623f544369900b0d31d4e835e
Jakob Pfender <jpfender@zedat.fu-berlin.de>
2015-10-01 15:16:00 +0200

    run_test: clean-up PYTHONPATH as all necessary dependencies are now installed on the ssh frontends

--- appli/iotlab/node_radio_characterization/run_characterization/run_test.sh
+++ appli/iotlab/node_radio_characterization/run_characterization/run_test.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 ROOT_IOTLAB="$(readlink -e $(git rev-parse --show-toplevel)/../../)"
 
-PYTHONPATH="${ROOT_IOTLAB}/tools_and_scripts/:${ROOT_IOTLAB}/cli-tools/:${ROOT_IOTLAB}/aggregation-tools/:."
+PYTHONPATH="."
 export PYTHONPATH
 
 experiment-cli get -r | python run_characterization.py $@
