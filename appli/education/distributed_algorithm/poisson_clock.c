0b3a40033ab1b54dcf028dffa14f6cd9dcf6e0b3
Gaëtan Harter <gaetan.harter@inria.fr>
2015-10-08 17:42:26 +0200

    Needed to add a two factor for lambda
    Don't know why but average was wrong. As if using lambda/2.

--- appli/education/distributed_algorithm/poisson_clock.c
+++ appli/education/distributed_algorithm/poisson_clock.c
@@ -9,9 +9,15 @@
 // STEP 1 tick
 #define STEP 1
 
+// Required to multiply by two...
+// I think it comes form the fact that
+//     one second == 2^15ticks and random is on 2^16
+// It might correct the probability
+#define MAGIC 2
+
 unsigned int poisson_delay_in_ticks(double lambda)
 {
-    double p = lambda * STEP;
+    double p = lambda * STEP * MAGIC;
     unsigned int delay = 0;
 
     while (random_rand16() >= p)
