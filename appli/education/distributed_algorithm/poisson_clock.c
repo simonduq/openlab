404a060e08ec1ede913067fcaa045e038efd7512
Gaëtan Harter <gaetan.harter@inria.fr>
2015-11-10 00:14:22 +0100

    Use 'log' to calculate lambda clock delay

--- appli/education/distributed_algorithm/poisson_clock.c
+++ appli/education/distributed_algorithm/poisson_clock.c
@@ -1,23 +1,17 @@
 #include <stdlib.h>
+#include <math.h>
+#include "soft_timer.h"
 #include "random.h"
 #include "poisson_clock.h"
 
-// STEP 1 tick
-#define STEP 1
-
-// Required to multiply by two...
-// I think it comes form the fact that
-//     one second == 2^15ticks and random is on 2^16
-// It might correct the probability
-#define MAGIC 2
 
 unsigned int poisson_step_ticks(double lambda)
 {
-    double p = lambda * STEP * MAGIC;
-    unsigned int delay = 0;
+    double rand = random_rand16() / ((float)(1<<16));
+    double delay;
 
-    while (random_rand16() >= p)
-        delay += STEP;
+    delay = -log(rand) / lambda;
+    delay *= SOFT_TIMER_FREQUENCY;  // in seconds
 
     return delay;
 }
